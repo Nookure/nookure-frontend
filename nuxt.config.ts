b51a6e9c3f46c2647d9e38cf5d4af8fa390f7892
Angelillo15 <contact@angelillo15.es>
2023-09-14 02:35:38 +0200

    Added modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/ui',
+    '@nuxt/image',
+  ],
 })
