e95947dbe1e13fa069084eaa7ba587b290d37650
Angelillo15 <contact@angelillo15.es>
2023-10-07 14:40:21 +0200

    Added tls

---
+++ lang/en.lang.ts
@@ -0,0 +1,35 @@
+export default {
+  index: {
+    meta: {
+      title: "Nookure - Home",
+      description: "Welcome to Nookure Studios Website, here you will find information about our projects and services.",
+    },
+    welcome: {
+      header: "Welcome to",
+      subHeader: "Where your ideas become code.",
+    },
+    features: {
+      scalability: {
+        header: "Scalability",
+        description: "Our solutions are designed to grow with your needs, ensuring long-term compatibility."
+      },
+      customization: {
+        header: "Customization",
+        description: "We offer a wide range of customization options to meet your needs."
+      },
+      security: {
+        header: "Security",
+        description: "We prioritize safety above all else and ensure quick solutions to any issues."
+      },
+      quality: {
+        header: "Quality",
+        description: "We are committed to providing the best quality solutions for our customers."
+      }
+    },
+  },
+  nav: {
+    home: "Home",
+    products: "Products",
+    docs: "Docs",
+  },
+}
---
+++ lang/es.lang.ts
@@ -0,0 +1,35 @@
+export default {
+  index: {
+    meta: {
+      title: "Nookure - Home",
+      description: "Welcome to Nookure Studios Website, here you will find information about our projects and services.",
+    },
+    welcome: {
+      header: "Bienvenido a",
+      subHeader: "Donde tus ideas se vuelven código.",
+    },
+    features: {
+      scalability: {
+        header: "Escalabilidad",
+        description: "Nuestras soluciones están diseñadas para crecer con sus necesidades, garantizando la compatibilidad a largo plazo."
+      },
+      customization: {
+        header: "Personalización",
+        description: "Ofrecemos una amplia gama de opciones de personalización para satisfacer sus necesidades."
+      },
+      security: {
+        header: "Seguridad",
+        description: "Priorizamos la seguridad por encima de todo y garantizamos soluciones rápidas para cualquier problema."
+      },
+      quality: {
+        header: "Calidad",
+        description: "Estamos comprometidos en proporcionar las mejores soluciones de calidad para nuestros clientes."
+      }
+    },
+  },
+  nav: {
+    home: "Inicio",
+    products: "Productos",
+    docs: "Docs",
+  },
+}
