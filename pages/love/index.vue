0163cd0dd759c063fbf7aa24ee63dcaf19b996ad
Angelillo15 <contact@angelillo15.es>
2023-10-07 14:50:33 +0200

    Update index.vue

--- pages/love/index.vue
+++ pages/love/index.vue
@@ -1,9 +1,5 @@
-<script lang="ts" setup>
-const love = "Ari <3"
-</script>
-
 <template>
   <div>
-    <h1 class="text-center text-6xl mt-3 font-bold"> {{ love }} </h1>
+    <h1 class="text-center text-6xl mt-3 font-bold"> Ari </h1>
   </div>
 </template>
