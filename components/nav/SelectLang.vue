359813b4fa8fbac62ea25ece7911e0147b8a414d
Angelillo15 <contact@angelillo15.es>
2023-10-07 14:42:37 +0200

    SelectLang

---
+++ components/nav/SelectLang.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const { locale } = useI18n();
+
+const en: lang = {
+  key: "en",
+  tl: "English",
+  flag: "🇺🇸",
+}
+
+const es: lang = {
+  key: "es",
+  tl: "Español",
+  flag: "🇪🇸",
+}
+
+interface lang {
+  key: string;
+  tl: string;
+  flag: string;
+}
+
+const languages = [en.tl, es.tl];
+const selected = ref(languages[0]);
+
+watch(selected, (value) => {
+  if (value === "English") {
+    locale.value = "en";
+  } else if (value === "Español") {
+    locale.value = "es";
+  }
+});
+
+</script>
+
+<template>
+  <USelectMenu searchable class="block" v-model="selected" :options="languages"/>
+</template>
