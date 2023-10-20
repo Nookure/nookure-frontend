febb0f32314a908f23d13406f7430a34d2610c25
Angelillo15 <contact@angelillo15.es>
2023-10-21 01:30:32 +0200

    create product data

---
+++ data/products.ts
@@ -0,0 +1,30 @@
+import type Product from "../interfaces/product";
+
+const products: Product[] = [
+  {
+    name: "MAStaff",
+    url: "https://builtbybit.com/resources/25460/",
+    description: "resource.mastaff.description",
+    image: "/resources/mastaff.png",
+  },
+  {
+    name: "NookTheme",
+    url: "https://builtbybit.com/resources/nooktheme-pterodactyl-theme.30893/",
+    description: "resource.nooktheme.description",
+    image: "/resources/nooktheme.png",
+  },
+  {
+    name: "Ice Minecraft Theme",
+    url: "https://builtbybit.com/resources/26274/",
+    description: "resource.ice.description",
+    image: "/resources/ice.png",
+  },
+  {
+    name: "Minecraft Purple Theme",
+    url: "https://pterodactyl-resources.com/resources/resource/43/",
+    description: "resource.purple.description",
+    image: "/resources/purple.png",
+  },
+];
+
+export default products;
