c5411cf69dbf6f0c282df2bcca4d4e9f7f88e79b
Angelillo15 <contact@angelillo15.es>
2024-09-04 20:16:35 +0200

    bump nuxt version and change build mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/i18n',
-    '@nuxt/ui',
-    '@nuxt/image',
-    'nuxt-twemoji'
-  ],
+
+  modules: ["@nuxtjs/i18n", "@nuxt/ui", "@nuxt/image"],
+
   colorMode: {
-    preference: 'dark'
+    preference: "dark",
   },
+
   i18n: {
-    vueI18n: './i18n.config.ts'
+    vueI18n: "./i18n.config.ts",
   },
-  ssr: true,
+
+  ssr: false,
+
   experimental: {
-    viewTransition: true
+    viewTransition: true,
   },
+
   nitro: {
-    preset: 'node-server'
-  }
-})
+    static: true,
+  },
+
+  compatibilityDate: "2024-09-04",
+});
